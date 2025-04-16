15479c515fc6491ce89c2b970e8c21ac87f3afd9
Miguel8pp <kinghostshop88@gmail.com>
2025-04-16 02:50:50 -0600

    musica

--- app.py
+++ app.py
@@ -2,20 +2,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def perdon():
-    return render_template('perdon.html')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/teamo')
+def teamo():
+    return render_template('teamo.html')
 
 @app.route('/pqteamo')
-def teamo():
+def pqteamo():
     return render_template('pqteamo.html')
 
 @app.route('/pqmgustas')
-def megustas():
+def pqmegustas():
     return render_template('pqmegustas.html')
 
 @app.route('/qmemolesta')
-def molesta():
+def qmolesta():
     return render_template('qmemolesta.html')
+
+@app.route('/canciones')
+def canciones():
+    return render_template('canciones.html')
+
 
 # Ejecutar la aplicación
 if __name__ == '__main__':
