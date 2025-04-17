ec4a1fd7934e582b6996b331d6980bcaabed568d
Miguel8pp <kinghostshop88@gmail.com>
2025-04-17 02:44:26 -0600

    mejoras en play colores y nuevas paginas

--- amor.py
+++ amor.py
@@ -7,16 +7,16 @@ def hearta(k):
 def heartb(k):
     return 12 * math.cos(k) - 5 * math.cos(2 * k) - 2 * math.cos(3 * k) - math.cos(4 * k)
 
-# Configuración inicial
+
 speed(0)
 bgcolor("black")
 color("red")
 penup()
 
-# Dibujo del corazón
+
 begin_fill()
 for i in range(600):
-    k = i * 0.01  # Convertimos i a radianes
+    k = i * 0.01 
     x = hearta(k) * 20
     y = heartb(k) * 20
     goto(x, y)
--- app.py
+++ app.py
@@ -1,5 +1,24 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+from urllib.parse import quote_plus
+
+load_dotenv()
+
 app = Flask(__name__)
+app.secret_key = os.getenv('FLASK_SECRET_KEY', 'advpjsh')
+socketio = SocketIO(app)  # Inicializa Flask-SocketIO
+
+# Conexión a MongoDB Atlas
+username = os.getenv('MONGO_USERNAME')
+password = os.getenv('MONGO_PASSWORD')
+username = quote_plus(username)
+password = quote_plus(password)
+client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.hx8un.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client['db1']
+messages_collection = db['mensajes_chat'] #define la coleccion de mensajes para el chat.
 
 @app.route('/')
 def inicio():
@@ -25,7 +44,24 @@ def qmolesta():
 def canciones():
     return render_template('canciones.html')
 
+@app.route('/tiempo')
+def tiempo():
+    return render_template('tiempo.html')
 
-# Ejecutar la aplicación
+@app.route('/rompe')
+def cabezas():
+    return render_template('rompecabezas.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+@socketio.on('send_message')
+def handle_message(data):
+    message = data['message']
+    messages_collection.insert_one({'message': message})
+    emit('receive_message', {'message': message}, broadcast=True)
+
+# Ejecutar la aplicación con SocketIO
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True) #usa socketio.run para ejecutar la app
